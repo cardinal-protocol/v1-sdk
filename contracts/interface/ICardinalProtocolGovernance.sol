8713c2dc5666fe33df8ae05ff4ced2569f62fbc4
harpoonjs <w3st.io2021@gmail.com>
2022-12-19 01:14:01 -0500

    Add interface

---
+++ contracts/interface/ICardinalProtocolGovernance.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/**
+* @title ICardinalProtocolGovernance
+*/
+interface ICardinalProtocolGovernance
+{
+	/**
+	* @notice AccessControl Role
+	* @return {bytes32} keccak256 value
+	*/
+	function S() external view returns (bytes32);
+
+	/**
+	* @notice AccessControl Role
+	* @return {bytes32} keccak256 value
+	*/
+	function A() external view returns (bytes32);
+
+	/**
+	* @notice AccessControl Role
+	* @return {bytes32} keccak256 value
+	*/
+	function B() external view returns (bytes32);
+
+	/**
+	* @notice AccessControl Role
+	* @return {bytes32} keccak256 value
+	*/
+	function C() external view returns (bytes32);
+}
