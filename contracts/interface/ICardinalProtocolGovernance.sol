3a181e650340b405ca3e0c18a9fe6d2e7d7492cc
harpoonjs <w3st.io2021@gmail.com>
2022-12-25 18:06:04 -0500

    Better comments

--- contracts/interface/ICardinalProtocolGovernance.sol
+++ contracts/interface/ICardinalProtocolGovernance.sol
@@ -13,25 +13,29 @@ interface ICardinalProtocolGovernance is
 	IAccessControlEnumerable
 {
 	/**
-	* @notice AccessControl Role
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
 	* @return {bytes32} keccak256 value
 	*/
 	function S() external view returns (bytes32);
 
 	/**
-	* @notice AccessControl Role
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
 	* @return {bytes32} keccak256 value
 	*/
 	function A() external view returns (bytes32);
 
 	/**
-	* @notice AccessControl Role
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
 	* @return {bytes32} keccak256 value
 	*/
 	function B() external view returns (bytes32);
 
 	/**
-	* @notice AccessControl Role
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
 	* @return {bytes32} keccak256 value
 	*/
 	function C() external view returns (bytes32);
