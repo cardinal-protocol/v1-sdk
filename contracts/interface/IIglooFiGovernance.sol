05fd11736f73bb875df4dba84b805970d5b686ee
aleem ahmed <w3st.io2021@gmail.com>
2023-01-26 22:57:37 -0500

    Remove unneeded spaces

--- contracts/interface/IIglooFiGovernance.sol
+++ contracts/interface/IIglooFiGovernance.sol
@@ -14,10 +14,8 @@ interface IIglooFiGovernance is
 {
 	/**
 	* @notice Governance Roles
-	*
 	* @dev [!restriction]
 	* @dev [view][mapping]
-	*
 	* @return {bytes32}
 	*/
 	function governanceRoles(string memory role)
@@ -28,11 +26,9 @@ interface IIglooFiGovernance is
 
 	/**
 	* @notice Adds a role to `_governanceRoles`
-	*
 	* @dev [restriction] AccessControlEnumerable → DEFAULT_ADMIN_ROLE 
 	* @dev [add] `_governanceRoles`
 	*      [update] `AccessControlEnumerable` → `_roles`
-	*
 	* @param role {string}
 	*/
 	function addGovernanceRole(string memory role)
