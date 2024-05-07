df4b2f7b4216e87430606457d5781c5ca74eac83
harppon <aleem.ahmed2015@gmail.com>
2024-05-06 20:26:20 -0400

    Updated

--- contracts/interface/ITransferRequestProtocol.sol
+++ contracts/interface/ITransferRequestProtocol.sol
@@ -7,61 +7,61 @@ struct TransferRequest
 	bool forERC20;
 	bool forERC721;
 	address creator;
-	address token;
-	uint256 tokenId;
-	uint256 amount;
 	address to;
+	address token;
+	uint256 amount;
+	uint256 created;
+	uint256 tokenId;
 }
 
 
 interface ITransferRequestProtocol
 {
 	/**
-	* @param yieldSyncV1VaultAddress {address}
-	* @param transferRequestId {uint256}
-	* @return {TransferRequest}
+	* @notice Getter for `_yieldSyncV1Vault_transferRequestId_transferRequest`
+	* @dev [view][mapping]
+	* @param _yieldSyncV1Vault {address}
+	* @param _transferRequestId {uint256}
 	*/
-	function yieldSyncV1VaultAddress_transferRequestId_transferRequest(
-		address yieldSyncV1VaultAddress,
-		uint256 transferRequestId
-	)
+	function yieldSyncV1Vault_transferRequestId_transferRequest(address _yieldSyncV1Vault, uint256 _transferRequestId)
 		external
-		view returns (TransferRequest memory)
+		view
+		returns (TransferRequest memory transferRequest_)
 	;
 
 	/**
-	* @param yieldSyncV1VaultAddress {address}
-	* @param transferRequestId {uint256}
+	* @param _yieldSyncV1Vault {address}
+	* @param _transferRequestId {uint256}
 	*/
-	function yieldSyncV1VaultAddress_transferRequestId_transferRequestProcess(
-		address yieldSyncV1VaultAddress,
-		uint256 transferRequestId
+	function yieldSyncV1Vault_transferRequestId_transferRequestProcess(
+		address _yieldSyncV1Vault,
+		uint256 _transferRequestId
 	)
 		external
 	;
 
 	/**
-	* @param yieldSyncV1VaultAddress {address}
-	* @param transferRequestId {uint256}
-	* @return readyToBeProcessed {bool}
-	* @return approved {bool}
-	* @return message {string}
+	* @param _yieldSyncV1Vault {address}
+	* @param _transferRequestId {uint256}
+	* @return readyToBeProcessed_ {bool}
+	* @return approved_ {bool}
+	* @return message_ {string}
 	*/
-	function yieldSyncV1VaultAddress_transferRequestId_transferRequestStatus(
-		address yieldSyncV1VaultAddress,
-		uint256 transferRequestId
+	function yieldSyncV1Vault_transferRequestId_transferRequestStatus(
+		address _yieldSyncV1Vault,
+		uint256 _transferRequestId
 	)
 		external
 		view
-		returns (bool readyToBeProcessed, bool approved, string memory message)
+		returns (bool readyToBeProcessed_, bool approved_, string memory message_)
 	;
 
 
 	/**
-	* @param initiator {address}
-	* @param yieldSyncV1VaultAddress {address}
+	* @param _initiator {address}
+	* @param _yieldSyncV1Vault {address}
 	*/
-	function yieldSyncV1VaultInitialize(address initiator, address yieldSyncV1VaultAddress)
+	function yieldSyncV1VaultInitialize(address _initiator, address _yieldSyncV1Vault)
 		external
 	;
 }
