aa55736e5610f9f732b257543a21d60bff578a7e
harppon <aleem.ahmed2015@gmail.com>
2024-05-14 20:31:33 -0400

    Updated

--- contracts/YieldSyncGovernance.sol
+++ contracts/YieldSyncGovernance.sol
@@ -2,42 +2,4 @@
 pragma solidity ^0.8.18;
 
 
-import { AccessControlEnumerable } from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-
-import { IYieldSyncGovernance } from "./interface/IYieldSyncGovernance.sol";
-
-
-contract YieldSyncGovernance is
-	AccessControlEnumerable,
-	IYieldSyncGovernance
-{
-	/// @inheritdoc IYieldSyncGovernance
-	address public override payTo;
-
-
-	constructor ()
-	{
-		_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-	}
-
-
-	/// @inheritdoc IYieldSyncGovernance
-	function payToUpdate(address _payTo)
-		public
-		override
-		onlyRole(DEFAULT_ADMIN_ROLE)
-	{
-		require(_payTo != address(0), "!(_payTo != address(0))");
-
-		payTo = _payTo;
-	}
-}
-
-/*
-* ██╗   ██╗██╗███████╗██╗     ██████╗     ███████╗██╗   ██╗███╗   ██╗ ██████╗
-* ╚██╗ ██╔╝██║██╔════╝██║     ██╔══██╗    ██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝
-*  ╚████╔╝ ██║█████╗  ██║     ██║  ██║    ███████╗ ╚████╔╝ ██╔██╗ ██║██║
-*   ╚██╔╝  ██║██╔══╝  ██║     ██║  ██║    ╚════██║  ╚██╔╝  ██║╚██╗██║██║
-*    ██║   ██║███████╗███████╗██████╔╝    ███████║   ██║   ██║ ╚████║╚██████╗
-*    ╚═╝   ╚═╝╚══════╝╚══════╝╚═════╝     ╚══════╝   ╚═╝   ╚═╝  ╚═══╝ ╚═════╝
-*/
+import { YieldSyncGovernance } from "@yield-sync/governance/contracts/YieldSyncGovernance.sol";
--- contracts/interface/IYieldSyncGovernance.sol
+++ contracts/interface/IYieldSyncGovernance.sol
@@ -2,27 +2,4 @@
 pragma solidity ^0.8.18;
 
 
-import { IAccessControlEnumerable } from "@openzeppelin/contracts/access/IAccessControlEnumerable.sol";
-
-
-interface IYieldSyncGovernance is
-	IAccessControlEnumerable
-{
-	/**
-	* @notice Pay To
-	*/
-	function payTo()
-		external
-		view
-		returns (address)
-	;
-
-
-	/**
-	* @notice Pay To Update
-	* @param _payTo {address}
-	*/
-	function payToUpdate(address _payTo)
-		external
-	;
-}
+import { IYieldSyncGovernance } from "@yield-sync/governance/contracts/interface/IYieldSyncGovernance.sol";
